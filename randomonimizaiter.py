8bffaca98992868e7e80be89dd58e40911ccb116
m1crocat <156919199+m1crocat@users.noreply.github.com>
2024-09-25 16:33:12 +0500

    1.1

--- randomonimizaiter.py
+++ randomonimizaiter.py
@@ -1,7 +1,8 @@
 def write_file(path, text):
     with open(path, "a") as z:
         z.write(str(text) + "\n")
-import random
+from random import randint
+from time import time
 mx = 0
 mn = 0
 total = 0
@@ -9,11 +10,12 @@ count = int(input("Введите, сколько чисел будет гене
 count2 = int(input("Введите, до скольки чисел будет генерироваться (от 1 до ..): "))
 lst = []
 test = True 
+start_time = time()
 for _ in range(count):
-        i = random.randint(1, count2)
+        i = randint(1, count2)
         if i in lst:
             while i in lst: 
-                i = random.randint(1, count2)
+                i = randint(1, count2)
         if i > mx:
             mx = i
             if test == True:
@@ -24,6 +26,7 @@ for _ in range(count):
         total += i
         lst.append(i)
 lst.sort(reverse=True)
+end_time = time()
 for i in lst:
     print(i)
     write_file("test.txt", i)
@@ -31,3 +34,4 @@ print("общее количество - ", count)
 print("максимальное - ",mx)
 print("минимальное -",mn)
 print("среднее число -", int(total / count))
+print("время -", round(end_time - start_time, 4))
