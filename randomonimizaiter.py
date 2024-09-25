58c905c838a980b03b5d59c8eb70fc9c83d9e420
m1crocat <156919199+m1crocat@users.noreply.github.com>
2024-09-25 17:35:02 +0500

    1.5

--- randomonimizaiter.py
+++ randomonimizaiter.py
@@ -13,17 +13,19 @@ total = 0
 test = True 
 RepeatOff = False
 numbs = []
-a = int(input("Введите 1, чтобы включить RepeatOff™ (отключение повтора цифр), или же 0 чтобы отключить: "))
+a = int(input("введите 1, чтобы включить RepeatOff™ (отключение повтора цифр), или же 0 чтобы отключить\nучтите, что при работе с большими числами это очень замедляет работу: "))
 if a == 1:
     RepeatOff = True
 elif a == 0:
     RepeatOff = False
 else:
     print("error!")
-count = int(input("Введите, сколько чисел будет генерироваться: "))
-count2 = int(input("Введите, до скольки чисел будет генерироваться (от 1 до ..): "))
-start_time = time()
-for _ in range(count):
+hello = int(input("1 - генерация\n2 - бенчмарк\n"))
+if hello == 1:
+    count = int(input("введите, сколько чисел будет генерироваться: "))
+    count2 = int(input("введите, до скольки чисел будет генерироваться (от 1 до ..): "))
+    start_time = time()
+    for _ in range(count):
         i = randint(1, count2)
         if RepeatOff:
             if i in numbs:
@@ -38,16 +40,40 @@ for _ in range(count):
             mn = i
         total += i
         numbs.append(i)
-numbs.sort(reverse=True)
-clear_file("test.txt")
-end_time = time()
-for i in numbs:
-    print(i)
-    write_file("test.txt", i)
-print("общее количество - ", count)
-print("максимальное - ",mx)
-print("минимальное -",mn)
-print("среднее число -", int(total / count))
-print("медиана -", numbs[count//2])
-print("размах -", numbs[0] - numbs[count - 1])
-print("время -", round(end_time - start_time, 3))
+    numbs.sort(reverse=True)
+    clear_file("test.txt")
+    end_time = time()
+    for i in numbs:
+        print(i)
+        write_file("test.txt", i)
+    print("общее количество - ", count)
+    print("максимальное - ",mx)
+    print("минимальное -",mn)
+    print("среднее число -", int(total / count))
+    print("медиана -", numbs[count//2])
+    print("размах -", numbs[0] - numbs[count - 1])
+    print("время -", round(end_time - start_time, 3))
+elif hello == 2:
+    input("этот бенчмарк покажет производительность вашего ПК в вычислениях. нажмите enter чтобы начать: ")
+    start_time = time()
+    bench = 0
+    for _ in range(3):
+        for _ in range(50000):
+                i = randint(1, 100000)
+                if RepeatOff:
+                    if i in numbs:
+                        while i in numbs: 
+                            i = randint(1, 200000)
+                if i > mx:
+                    mx = i
+                    if test == True:
+                        mn = mx
+                        test = False
+                if i < mn:
+                    mn = i
+                total += i
+                numbs.append(i)
+        numbs.sort(reverse=True)
+        end_time = time()
+        bench += end_time - start_time
+    print("результат -", round(bench / 3 , 4) , ", RepeatOff™ -", RepeatOff)
