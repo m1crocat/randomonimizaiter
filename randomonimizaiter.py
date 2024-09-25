d92ddd4a5e37570ee31a8d7fe9f652243a8420d0
m1crocat <156919199+m1crocat@users.noreply.github.com>
2024-09-25 17:10:59 +0500

    1.4

--- randomonimizaiter.py
+++ randomonimizaiter.py
@@ -6,20 +6,29 @@ def clear_file(path):
         z.write("")
 from random import randint
 from time import time
-print("randomonimizaiter v1.3 by m1cro_cat")
+print("randomonimizaiter v1.4 by m1cro_cat")
 mx = 0
 mn = 0
 total = 0
+test = True 
+RepeatOff = False
+numbs = []
+a = int(input("Введите 1, чтобы включить RepeatOff™ (отключение повтора цифр), или же 0 чтобы отключить: "))
+if a == 1:
+    RepeatOff = True
+elif a == 0:
+    RepeatOff = False
+else:
+    print("error!")
 count = int(input("Введите, сколько чисел будет генерироваться: "))
 count2 = int(input("Введите, до скольки чисел будет генерироваться (от 1 до ..): "))
-lst = []
-test = True 
 start_time = time()
 for _ in range(count):
         i = randint(1, count2)
-        if i in lst:
-            while i in lst: 
-                i = randint(1, count2)
+        if RepeatOff:
+            if i in numbs:
+                while i in numbs: 
+                    i = randint(1, count2)
         if i > mx:
             mx = i
             if test == True:
@@ -28,17 +37,17 @@ for _ in range(count):
         if i < mn:
             mn = i
         total += i
-        lst.append(i)
-lst.sort(reverse=True)
+        numbs.append(i)
+numbs.sort(reverse=True)
 clear_file("test.txt")
 end_time = time()
-for i in lst:
+for i in numbs:
     print(i)
     write_file("test.txt", i)
 print("общее количество - ", count)
 print("максимальное - ",mx)
 print("минимальное -",mn)
 print("среднее число -", int(total / count))
-print("медиана -", lst[count//2])
-print("размах -", lst[0] - lst[count - 1])
+print("медиана -", numbs[count//2])
+print("размах -", numbs[0] - numbs[count - 1])
 print("время -", round(end_time - start_time, 3))
